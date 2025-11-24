55f6c0f4bc17f869046d6f09f4ce4b16dd35f7ff
Bivas-Biswas <bivasbiswas1999@gmail.com>
2025-11-24 15:45:50 +0530

    🔧 added: cros suppor for all orgin

--- src/config/fastify.ts
+++ src/config/fastify.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
 import fastifyWebsocket from "@fastify/websocket";
+import cors from "@fastify/cors";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 export function buildFastify() {
   const fastify = Fastify({ logger: true }).withTypeProvider<TypeBoxTypeProvider>();
   fastify.register(fastifyWebsocket);
+  fastify.register(cors, {
+    origin: "*"
+  })
   return fastify;
 }
